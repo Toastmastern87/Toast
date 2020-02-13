b8859b0fd2ce7af4cf2cf723947c8553a8b8115b
Johan Karlsson <JohanX.Karlsson@Gmail.com>
2020-02-13 19:15:30 +0100

    Setup basic logger for Toast and client.

---
+++ Toast/src/Toast/Log.cpp
@@ -0,0 +1,18 @@
+#include "Log.h"
+
+#include "spdlog/sinks/stdout_color_sinks.h"
+
+namespace Toast 
+{
+	std::shared_ptr<spdlog::logger> Log::s_CoreLogger;
+	std::shared_ptr<spdlog::logger> Log::s_ClientLogger;
+
+	void Log::Init() 
+	{
+		spdlog::set_pattern("%^[%T] %n: %v%$");
+		s_CoreLogger = spdlog::stdout_color_mt("TOAST");
+		s_CoreLogger->set_level(spdlog::level::trace);
+		s_ClientLogger = spdlog::stdout_color_mt("APP");
+		s_ClientLogger->set_level(spdlog::level::trace);
+	}
+}
