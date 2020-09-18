cac3367d59049d48fb122693ae6c1b614d604311
Johan Karlsson <JohanX.Karlsson@Gmail.com>
2020-09-18 18:08:11 +0200

    Toaster * Added Camera Component and Sprite Render Components to the Property Panel

--- Toast/src/Toast/Scene/SceneCamera.h
+++ Toast/src/Toast/Scene/SceneCamera.h
@@ -18,10 +18,21 @@ namespace Toast {
 		void SetViewportSize(uint32_t width, uint32_t height);
 
 		void SetPerspectiveVerticalFOV(float verticalFOV) { mPerspectiveFOV = DirectX::XMConvertToRadians(verticalFOV); };
-		float GetPerspectiveVerticalFOV() const { return mPerspectiveFOV; }
+		float GetPerspectiveVerticalFOV() const { return DirectX::XMConvertToDegrees(mPerspectiveFOV); }
+		void SetPerspectiveNearClip(float nearClip) { mPerspectiveNear = nearClip; };
+		float GetPerspectiveNearClip() const { return mPerspectiveNear; }
+		void SetPerspectiveFarClip(float farClip) { mPerspectiveFar = farClip; };
+		float GetPerspectiveFarClip() const { return mPerspectiveFar; }
 
 		float GetOrthographicSize() const { return mOrthographicSize; }
 		void SetOrthographicSize(float size) { mOrthographicSize = std::max(size, 0.1f); RecalculateProjection(); }
+		void SetOrthographicNearClip(float nearClip) { mOrthographicNear = nearClip; };
+		float GetOrthographicNearClip() const { return mOrthographicNear; }
+		void SetOrthographicFarClip(float farClip) { mOrthographicFar = farClip; };
+		float GetOrthographicFarClip() const { return mOrthographicFar; }
+
+		ProjectionType GetProjectionType() const { return mProjectionType; }
+		void SetProjectionType(ProjectionType type) { mProjectionType = type; }
 	private:
 		void RecalculateProjection();
 	private:
