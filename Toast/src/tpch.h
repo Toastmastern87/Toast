78d5a492c722bf9d8b85e33168e3e036d79dabc7
Toastmastern <johanx.karlsson@gmail.com>
2022-05-30 20:13:41 +0200

    General * Added alot of stuff to my precompiled header so that DirectXTK didn't need a big header file aswell but can just use Toast Engines one

--- Toast/src/tpch.h
+++ Toast/src/tpch.h
@@ -1,5 +1,31 @@
 #pragma once
 
+// Off by default warnings
+#pragma warning(disable : 4619 4616 4061 4265 4365 4571 4623 4625 4626 4628 4668 4710 4711 4746 4774 4820 4987 5026 5027 5031 5032 5039 5045 5219 26812)
+// C4619/4616 #pragma warning warnings
+// C4061 enumerator 'X' in switch of enum 'X' is not explicitly handled by a case label
+// C4265 class has virtual functions, but destructor is not virtual
+// C4365 signed/unsigned mismatch
+// C4571 behavior change
+// C4623 default constructor was implicitly defined as deleted
+// C4625 copy constructor was implicitly defined as deleted
+// C4626 assignment operator was implicitly defined as deleted
+// C4628 digraphs not supported
+// C4668 not defined as a preprocessor macro
+// C4710 function not inlined
+// C4711 selected for automatic inline expansion
+// C4746 volatile access of '<expression>' is subject to /volatile:<iso|ms> setting
+// C4774 format string expected in argument 3 is not a string literal
+// C4820 padding added after data member
+// C4987 nonstandard extension used
+// C5026 move constructor was implicitly defined as deleted
+// C5027 move assignment operator was implicitly defined as deleted
+// C5031/5032 push/pop mismatches in windows headers
+// C5039 pointer or reference to potentially throwing function passed to extern C function under - EHc
+// C5045 Spectre mitigation warning
+// C5219 implicit conversion from 'int' to 'float', possible loss of data
+// 26812: The enum type 'x' is unscoped. Prefer 'enum class' over 'enum' (Enum.3).
+
 #ifdef TOAST_PLATFORM_WINDOWS
 	#ifndef NOMINMAX
 		// See github.com/skypjack/entt/wiki/Frequently-Asked-Questions#warning-c4003-the-min-the-max-and-the-macro
@@ -10,6 +36,26 @@
 	#include <windowsx.h>
 #endif
 
+#pragma warning(push)
+#pragma warning(disable : 4005)
+#define NOMINMAX
+#define NODRAWTEXT
+#define NOGDI
+#define NOBITMAP
+#define NOMCX
+#define NOSERVICE
+#define NOHELP
+#pragma warning(pop)
+
+//#define _XM_NO_XMVECTOR_OVERLOADS_
+
+#include <DirectXMath.h>
+#include <DirectXPackedVector.h>
+#include <DirectXCollision.h>
+
+#include <algorithm>
+#include <cstddef>
+#include <cstdint>
 #include <iostream>
 #include <memory>
 #include <utility>
@@ -18,12 +64,33 @@
 #include <array>
 #include <chrono>
 #include <limits>
+#include <list>
+#include <map>
+#include <set>
+#include <stdexcept>
+#include <utility>
 
 #include <string>
 #include <sstream>
 #include <vector>
 #include <unordered_map>
 #include <unordered_set>
+
+#pragma warning(push)
+#pragma warning(disable : 4702)
+#include <functional>
+#pragma warning(pop)
+
+#include <malloc.h>
+
+#pragma warning(push)
+#pragma warning(disable : 4467 5038 5204 5220)
+#include <wrl.h>
+#pragma warning(pop)
+
+#include <wincodec.h>
+
+#include <mutex>
 
 #include "Toast/Core/Base.h"
 
